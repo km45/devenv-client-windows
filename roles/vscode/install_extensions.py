9ef224e0fc1b46d2dbfdd0c50c404c9c6666fdf8
Kamei Shogo <km45@users.noreply.github.com>
2019-10-27 04:56:20 +0900

    Refactor

--- roles/vscode/install_extensions.py
+++ roles/vscode/install_extensions.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python3
 import os
 import subprocess
-from logging import INFO, Formatter, Logger, StreamHandler, getLogger
+from logging import Logger
 
 import yaml
 
+import log
 
-def create_default_logger() -> Logger:
-    LOG_LEVEL = INFO
-    LOG_FORMAT = "[%(asctime)s] [%(levelname)s] (%(filename)s:%(lineno)d) %(message)s"
-
-    handler = StreamHandler()
-    handler.setLevel(LOG_LEVEL)
-    handler.setFormatter(Formatter(LOG_FORMAT))
-
-    logger = getLogger(__name__)
-    logger.setLevel(LOG_LEVEL)
-    logger.addHandler(handler)
-
-    return logger
-
-
-DEFAULT_LOGGER: Logger = create_default_logger()
+DEFAULT_LOGGER: Logger = log.create_default_logger()
 
 
 def is_installed(extension: str, *, logger: Logger = DEFAULT_LOGGER) -> bool:
