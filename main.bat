a3e2e65225c4c839e8c023d90ad6776daea36049
Kamei Shogo <km45@users.noreply.github.com>
2020-05-24 19:21:05 +0900

    Add mode parameter
    Resolve #9

--- main.bat
+++ main.bat
@@ -4,8 +4,16 @@
 setlocal
     set WORKDIR=%~dp0
 
+    if "%1" == "" (
+        call :LogError "You must specify mode as argument-1."
+        exit /b 1
+    )
+
     REM "Execute the role scoop at first"
-    call %WORKDIR%\roles\scoop\main.bat
+    call %WORKDIR%\roles\scoop\main.bat %1
+    if %ERRORLEVEL% == 1 (
+        exit /b
+    )
 
     REM "Execute the role python at second"
     call %WORKDIR%\roles\python\main.bat
@@ -15,5 +23,22 @@ setlocal
     call %WORKDIR%\roles\terminus\main.bat
     call %WORKDIR%\roles\vscode\main.bat
 
+    exit /b
+endlocal
+
+:LogError
+setlocal
+    call :LogImpl E %*
+    exit /b
+endlocal
+
+:LogImpl
+setlocal
+    set /p NOP=%date% < nul
+    set /p NOP=%time% < nul
+    for %%s in (%*) do (
+        set /p NOP=%%~s < nul
+    )
+    echo;
     exit /b
 endlocal
