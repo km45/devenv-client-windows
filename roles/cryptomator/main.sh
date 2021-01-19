4df1e447311c3ea8c87c2d1139d469e96f6f1259
Kamei Shogo <km45@users.noreply.github.com>
2021-01-20 03:19:01 +0900

    Fix script in cryptomator role

--- roles/cryptomator/main.sh
+++ roles/cryptomator/main.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eu
 
+function LOG_impl() {
+    local -r loglevel=$1
+    local -r messages=${*:2}
+    local -r timestamp=$(date +'%Y/%m/%d %H:%M:%S.%3N')
+    echo "[${timestamp}] ${loglevel} ${messages}"
+}
+
 function LOGI() {
     local -r messages=$*
     local -r LEVEL='I'
