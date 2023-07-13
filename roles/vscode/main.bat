a0efbd76b41eff4d5fe3c9bb069c5386cb658b2d
Kamei Shogo <km45@users.noreply.github.com>
2023-07-13 19:14:45 +0900

    Manage vscode extension list in this repository

--- roles/vscode/main.bat
+++ roles/vscode/main.bat
@@ -6,7 +6,6 @@ setlocal
     cd /d %WORKDIR%
 
     python -m poetry install
-    python -m poetry run python download.py "https://raw.githubusercontent.com/km45/linux-devenv/master/src/playbooks/roles/vscode/vars/main.yml" vscode_extensions.yml
     python -m poetry run python install_extensions.py vscode_extensions.yml
     python -m poetry run python change_user_settings.py vscode_extensions.yml
 
