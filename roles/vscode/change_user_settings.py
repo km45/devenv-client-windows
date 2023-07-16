15d824260bf080303fc2f202b26bd48b74142a17
Kamei Shogo <km45@users.noreply.github.com>
2023-07-16 16:50:16 +0900

    Change vscode settings for teminal to work with vscode 1.80.0 or later

--- roles/vscode/change_user_settings.py
+++ roles/vscode/change_user_settings.py
@@ -42,11 +42,6 @@ def main():
         with open(output_path, mode="w") as f:
             logger.info(f"Open user settings.json: {output_path}")
 
-            shell_path = os.path.join(
-                scoopdir, "apps", "git-with-openssh", "current", "bin", "bash.exe"
-            )
-            shell_args = ["--login", "-i"]
-
             remote_ssh_config_path = os.path.join(
                 homedir, ".ssh", "config.d", "vscode-remote-ssh"
             )
@@ -68,8 +63,7 @@ def main():
                     "remote.SSH.configFile": remote_ssh_config_path,
                     "remote.SSH.defaultExtensions": remote_ssh_default_extensions,
                     "terminal.integrated.allowChords": False,
-                    "terminal.integrated.shell.windows": shell_path,
-                    "terminal.integrated.shellArgs.windows": shell_args,
+                    "terminal.integrated.defaultProfile.windows": "Git Bash",
                 },
                 ensure_ascii=False,
                 indent=4,
