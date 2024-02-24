17dcfb1f2992e166c5dedf03445ef453e8957c91
Kamei Shogo <km45@users.noreply.github.com>
2024-02-25 00:47:33 +0900

    Install VSCode extensions without Python

--- roles/vscode/main.sh
+++ roles/vscode/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -eu -o pipefail
 
 readonly VENV_DIR_NAME=venv
 
@@ -16,9 +16,40 @@ function LOGI() {
         LOG_impl ${LEVEL} "${messages}"
 }
 
+function install_extension() {
+        local -r extension_id=$1
+
+        set +e
+        code --list-extensions | grep --line-regexp --fixed-strings "${extension_id}" >/dev/null
+        local -r ret=$?
+        set -e
+
+        case ${ret} in
+        0)
+                LOGI "Already installed ${extension_id}"
+                return
+                ;;
+        1)
+                # break
+                ;;
+        *)
+                LOGE "An unexpected error occurred!"
+                exit 1
+                ;;
+        esac
+
+        LOGI "Install ${extension_id}"
+        code --install-extension "${extension_id}"
+}
+
 function main() {
         local -r workdir=$(dirname "$0")
         cd "${workdir}"
+
+        LOGI "Install vscode extensions"
+        yq .vscode_plugins[] <vscode_extensions.yml | while read -r extension_id; do
+                install_extension "${extension_id}"
+        done
 
         if [ ! -e ${VENV_DIR_NAME}/Scripts/activate ]; then
                 LOGI "Create new venv"
@@ -27,9 +58,6 @@ function main() {
 
         LOGI "Activate venv and install packages"
         . ${VENV_DIR_NAME}/Scripts/activate && python -m pip install -r requirements.txt
-
-        LOGI "Install vscode extensions"
-        python -m install_extensions vscode_extensions.yml
 
         LOGI "Change vscode user settings"
         python -m change_user_settings vscode_extensions.yml
